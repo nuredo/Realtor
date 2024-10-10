c705ed154625da84b27f59f60d0502f1cab41626
nuredo <hammednuredo@gmail.com>
2024-10-10 03:26:59 -0700

    install reactjs and tailwindcss

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <dv>
+        hello
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </dv>
+    </div>
+  );
+}
+
+export default App;
